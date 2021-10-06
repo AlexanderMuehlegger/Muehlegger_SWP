8d53df003dcf1480587210d19d92ffc86d022197
Alexander Mühlegger <amuehlegger@tsn.at>
2021-10-06 19:54:17 +0200

    Update binarySearch.java
    Zeitmessung hinzugefügt

--- binarySearch.java
+++ binarySearch.java
@@ -1,55 +1,65 @@
+import java.sql.Date;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
-public class binarySearch{
-    
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int toFind = -1;
-        int[] zahlen = {1,2,3,5,5,8,9,9,15,22,32,51,52,53,60,64,65,66,87,354,502};
-        String result;
+public class binarySearch {
 
-        System.out.print("Geben Sie die zu suchende Zahl ein:");
-        
-        try{
-            toFind = Integer.parseInt(scanner.nextLine());
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int toFind = -1;
+    int[] zahlen = { 1, 2, 3, 5, 5, 8, 9, 9, 15, 22, 32, 51, 52, 53, 60, 64, 65, 66, 87, 354, 502 };
+    String result;
+    long timeStart, timeStop;
 
-        result = String.valueOf(iterativSearch(zahlen, toFind)+1);
+    while (true) {
+      System.out.print("Geben Sie die zu suchende Zahl ein:");
 
-        if(result.equals("-1")){
-            System.out.println("Die Zahl ist im Array nicht vorhanden!");
-        }else{
-            System.out.printf("\nDie Zahl: %d wurde am Index: %s gefunden!", toFind, result);
-        }
-
-        scanner.close();
-    }
-
-    public static int iterativSearch(int[] array, int toFind) {
-        return iterativSearch(array, 0, array.length, toFind);
-    }
-      
-      public static int iterativSearch(int[] array, int fromIndex, int toIndex, int toFind) {
-        int low = fromIndex;
-        int high = toIndex;
-      
-        while (low < high) {
-          int mid = (low + high) >>> 1;
-          int midVal = array[mid];
-      
-          if (toFind == midVal) {
-            return mid;
-          } else if (toFind < midVal) {
-            high = mid;
-          } else {
-            low = mid + 1;
-          }
-        }
-
-        return -1;
+      try {
+        toFind = Integer.parseInt(scanner.nextLine());
+      } catch (Exception e) {
+        e.printStackTrace();
       }
+
+      timeStart = System.currentTimeMillis();
+      result = String.valueOf(iterativSearch(zahlen, toFind) + 1);
+      timeStop = System.currentTimeMillis();
+
+      if (result.equals("-1")) {
+        System.out.println("Die Zahl ist im Array nicht vorhanden!");
+      } else {
+        System.out.printf("\nDie Zahl: %d wurde am Index: %s gefunden!", toFind, result);
+        System.out.printf("\nDie Suche hat %d Millisekunden gedauert.", (timeStop - timeStart));
+        break;
+      }
+    }
+
+    scanner.close();
+
+  }
+
+  public static int iterativSearch(int[] array, int toFind) {
+    return iterativSearch(array, 0, array.length, toFind);
+  }
+
+  public static int iterativSearch(int[] array, int fromIndex, int toIndex, int toFind) {
+    int low = fromIndex;
+    int high = toIndex;
+
+    while (low < high) {
+      int mid = (low + high) >>> 1;
+      int midVal = array[mid];
+
+      if (toFind == midVal) {
+        return mid;
+      } else if (toFind < midVal) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    return -1;
+  }
 
 }
