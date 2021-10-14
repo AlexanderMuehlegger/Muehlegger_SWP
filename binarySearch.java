496d318bc0cb390a0c690fb4ba7267e9493e89a3
Alexander Mühlegger <amuehlegger@tsn.at>
2021-10-14 08:56:18 +0200

    Update binarySearch.java

--- binarySearch.java
+++ binarySearch.java
@@ -8,28 +8,41 @@ public class binarySearch {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int toFind = -1;
-    int[] zahlen = { 1, 2, 3, 5, 5, 8, 9, 9, 15, 22, 32, 51, 52, 53, 60, 64, 65, 66, 87, 354, 502 };
-    String result;
+    int[] zahlen;
+    String result, result2;
     long timeStart, timeStop;
+
+    System.out.print("Geben sie die Länge der Array ein: ");
+    int length = scanner.nextInt();
+    zahlen = new int[length];
+
+    for(int i = 0; i < length; i++){
+      zahlen[i] = i;
+    }
+    
+    System.out.println("Array erstellt!");
 
     while (true) {
       System.out.print("Geben Sie die zu suchende Zahl ein:");
 
       try {
-        toFind = Integer.parseInt(scanner.nextLine());
+        toFind = Integer.parseInt(scanner.next());
       } catch (Exception e) {
         e.printStackTrace();
       }
 
       timeStart = System.currentTimeMillis();
-      result = String.valueOf(iterativSearch(zahlen, toFind) + 1);
+      result = String.valueOf(iterativBinarySearch(zahlen, toFind) + 1);
       timeStop = System.currentTimeMillis();
+
+      result2 = String.valueOf(iterativeSearch(zahlen, toFind));
 
       if (result.equals("-1")) {
         System.out.println("Die Zahl ist im Array nicht vorhanden!");
       } else {
         System.out.printf("\nDie Zahl: %d wurde am Index: %s gefunden!", toFind, result);
-        System.out.printf("\nDie Suche hat %d Millisekunden gedauert.", (timeStop - timeStart));
+        System.out.printf("\nDie Binäre Suche hat %d Millisekunden benötigt.", (timeStop - timeStart));
+        System.out.printf("\nDie iterative Suche hat %s Millisekunden benötigt.", result2);
         break;
       }
     }
@@ -38,11 +51,11 @@ public class binarySearch {
 
   }
 
-  public static int iterativSearch(int[] array, int toFind) {
-    return iterativSearch(array, 0, array.length, toFind);
+  public static int iterativBinarySearch(int[] array, int toFind) {
+    return iterativBinarySearch(array, 0, array.length, toFind);
   }
 
-  public static int iterativSearch(int[] array, int fromIndex, int toIndex, int toFind) {
+  public static int iterativBinarySearch(int[] array, int fromIndex, int toIndex, int toFind) {
     int low = fromIndex;
     int high = toIndex;
 
@@ -60,6 +73,25 @@ public class binarySearch {
     }
 
     return -1;
+  }
+
+
+  private static int iterativeSearch(int[] array, int toFind){
+    boolean found = false;
+    long timeStart = System.currentTimeMillis();
+    for(int i = 0; i < array.length; i++){
+      if(array[i] == toFind){
+        found = true;
+        break;
+      }
+    }
+    long timeStop = System.currentTimeMillis();
+
+    if(found){
+      return (int)(timeStop - timeStart);
+    }else{
+      return -1;
+    }
   }
 
 }
